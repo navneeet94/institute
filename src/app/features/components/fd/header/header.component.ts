d2202720472d7f0961edc6078237a6d102572a5f
AjitP <Ajitbpisal@sbimf.com>
2022-08-21 11:49:58 +0530

    update code

--- src/app/features/components/fd/header/header.component.ts
+++ src/app/features/components/fd/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  headerOpt1: boolean = true ;
+  headerOpt2: boolean = false ;
+
   constructor() { }
 
   ngOnInit(): void {
